1c5bf73531bdae78eec6f998ad6f4a555c447cda
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2018-11-18 13:31:40 +0100

    Changed tag scheme. Changed base image to alpine. Using ruby build and deploy script. Support for latest versions of helm, kubectl and terraform.

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # docker-helm-kubectl-terraform
-A Docker image that includes kubectl, helm and terraform.
+A Docker image that includes helm, kubectl, and terraform.
 
 [![Build Status](https://travis-ci.org/ngeor/docker-helm-kubectl-terraform.svg?branch=master)](https://travis-ci.org/ngeor/docker-helm-kubectl-terraform)
 
-- Builds on top of `dtzar/helm-kubectl:2.8.2`
-- Includes `terraform` 0.11.8
+- Inspired by [dtzar/helm-kubectl](https://github.com/dtzar/helm-kubectl)
+- Includes `terraform`
 - Docker image is published at https://hub.docker.com/r/ngeor/helm-kubectl-terraform/
+
+## Tags
+
+The tag scheme is a bit complex because of the multiple packages.
+
+`helm-version__kubectl-version__terraform-version`
+
+e.g.
+
+`2.8.2__0.11.3__0.11.0`
---
+++ build.rb
@@ -0,0 +1,70 @@
+# Build and deploy script
+
+def run(cmd)
+    success = system(cmd)
+    abort('Could not execute command') unless success
+end
+
+def login
+    # Login to docker
+    run("echo #{ENV['DOCKER_PASSWORD']} | docker login -u #{ENV['DOCKER_USERNAME']} --password-stdin")
+end
+
+def logout
+    # Logout from Docker
+    run('docker logout')
+end
+
+def build(dirs, versions)
+    dirs.each do |dir|
+        Dir.chdir(dir) do
+            versions.each do |version|
+                puts "Building version #{version}"
+                tag = "#{version[:helm]}__#{version[:kubectl]}__#{version[:terraform]}".gsub('v', '')
+                buildArgs = "--build-arg HELM_VERSION=#{version[:helm]}" + \
+                    " --build-arg KUBECTL_VERSION=#{version[:kubectl]}" + \
+                    " --build-arg TERRAFORM_VERSION=#{version[:terraform]}"
+                image_name = "#{dir}:#{tag}"
+                run("docker build #{buildArgs} -t #{image_name} .")
+            end
+        end
+    end
+end
+
+def deploy(dirs, versions)
+    dirs.each do |dir|
+        versions.each do |version|
+            puts "Deploying version #{version}"
+            tag = "#{version[:helm]}__#{version[:kubectl]}__#{version[:terraform]}".gsub('v', '')
+            image_name = "#{dir}:#{tag}"
+            public_image_name = "#{ENV['DOCKER_USERNAME']}/#{image_name}"
+            run("docker tag #{image_name} #{public_image_name}")
+            run("docker push #{public_image_name}")
+        end
+    end
+end
+
+helm_versions = ['v2.8.0', 'v2.8.2', 'v2.11.0']
+kubectl_versions = ['v1.11.3', 'v1.12.0']
+terraform_versions = ['0.11.8', '0.11.10']
+
+versions = []
+
+helm_versions.each do |h|
+    kubectl_versions.each do |k|
+        terraform_versions.each do |t|
+            versions.push({ helm: h, kubectl: k, terraform: t})
+        end
+    end
+end
+
+# find all top level folders, except hidden
+dirs = Dir.glob('*').select { |f| File.directory? f }
+
+if ARGV[0] == 'deploy'
+    login
+    deploy(dirs, versions)
+    logout
+else
+    build(dirs, versions)
+end
