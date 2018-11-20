6316a7b617a2a06ca2753238dd8ede2ce3535bb2
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2018-11-20 21:08:25 +0100

    Added ruby flavor.

--- README.md
+++ README.md
@@ -16,3 +16,41 @@ The tag scheme is a bit complex because of the multiple packages.
 e.g.
 
 `2.8.2__0.11.3__0.11.0`
+
+### helm-kubectl-terraform
+
+Based on alpine.
+
+| Name                                           | helm    | kubectl | terraform |
+|------------------------------------------------|---------|---------|-----------|
+| helm-kubectl-terraform:2.8.0__1.11.3__0.11.8   | v2.8.0  | v1.11.3 | 0.11.8    |
+| helm-kubectl-terraform:2.8.0__1.11.3__0.11.10  | v2.8.0  | v1.11.3 | 0.11.10   |
+| helm-kubectl-terraform:2.8.0__1.12.0__0.11.8   | v2.8.0  | v1.12.0 | 0.11.8    |
+| helm-kubectl-terraform:2.8.0__1.12.0__0.11.10  | v2.8.0  | v1.12.0 | 0.11.10   |
+| helm-kubectl-terraform:2.8.2__1.11.3__0.11.8   | v2.8.2  | v1.11.3 | 0.11.8    |
+| helm-kubectl-terraform:2.8.2__1.11.3__0.11.10  | v2.8.2  | v1.11.3 | 0.11.10   |
+| helm-kubectl-terraform:2.8.2__1.12.0__0.11.8   | v2.8.2  | v1.12.0 | 0.11.8    |
+| helm-kubectl-terraform:2.8.2__1.12.0__0.11.10  | v2.8.2  | v1.12.0 | 0.11.10   |
+| helm-kubectl-terraform:2.11.0__1.11.3__0.11.8  | v2.11.0 | v1.11.3 | 0.11.8    |
+| helm-kubectl-terraform:2.11.0__1.11.3__0.11.10 | v2.11.0 | v1.11.3 | 0.11.10   |
+| helm-kubectl-terraform:2.11.0__1.12.0__0.11.8  | v2.11.0 | v1.12.0 | 0.11.8    |
+| helm-kubectl-terraform:2.11.0__1.12.0__0.11.10 | v2.11.0 | v1.12.0 | 0.11.10   |
+
+### ruby-helm-kubectl-terraform
+
+Based on ruby:2.5.3-alpine
+
+| Name                                                | helm    | kubectl | terraform |
+|-----------------------------------------------------|---------|---------|-----------|
+| ruby-helm-kubectl-terraform:2.8.0__1.11.3__0.11.8   | v2.8.0  | v1.11.3 | 0.11.8    |
+| ruby-helm-kubectl-terraform:2.8.0__1.11.3__0.11.10  | v2.8.0  | v1.11.3 | 0.11.10   |
+| ruby-helm-kubectl-terraform:2.8.0__1.12.0__0.11.8   | v2.8.0  | v1.12.0 | 0.11.8    |
+| ruby-helm-kubectl-terraform:2.8.0__1.12.0__0.11.10  | v2.8.0  | v1.12.0 | 0.11.10   |
+| ruby-helm-kubectl-terraform:2.8.2__1.11.3__0.11.8   | v2.8.2  | v1.11.3 | 0.11.8    |
+| ruby-helm-kubectl-terraform:2.8.2__1.11.3__0.11.10  | v2.8.2  | v1.11.3 | 0.11.10   |
+| ruby-helm-kubectl-terraform:2.8.2__1.12.0__0.11.8   | v2.8.2  | v1.12.0 | 0.11.8    |
+| ruby-helm-kubectl-terraform:2.8.2__1.12.0__0.11.10  | v2.8.2  | v1.12.0 | 0.11.10   |
+| ruby-helm-kubectl-terraform:2.11.0__1.11.3__0.11.8  | v2.11.0 | v1.11.3 | 0.11.8    |
+| ruby-helm-kubectl-terraform:2.11.0__1.11.3__0.11.10 | v2.11.0 | v1.11.3 | 0.11.10   |
+| ruby-helm-kubectl-terraform:2.11.0__1.12.0__0.11.8  | v2.11.0 | v1.12.0 | 0.11.8    |
+| ruby-helm-kubectl-terraform:2.11.0__1.12.0__0.11.10 | v2.11.0 | v1.12.0 | 0.11.10   |
--- build.rb
+++ build.rb
@@ -2,47 +2,65 @@
 # Build and deploy script
 
 def run(cmd)
-    success = system(cmd)
-    abort('Could not execute command') unless success
+  success = system(cmd)
+  abort('Could not execute command') unless success
 end
 
 def login
-    # Login to docker
-    run("echo #{ENV['DOCKER_PASSWORD']} | docker login -u #{ENV['DOCKER_USERNAME']} --password-stdin")
+  # Login to docker
+  run("echo #{ENV['DOCKER_PASSWORD']} | docker login -u #{ENV['DOCKER_USERNAME']} --password-stdin")
 end
 
 def logout
-    # Logout from Docker
-    run('docker logout')
+  # Logout from Docker
+  run('docker logout')
 end
 
 def build(dirs, versions)
-    dirs.each do |dir|
-        Dir.chdir(dir) do
-            versions.each do |version|
-                puts "Building version #{version}"
-                tag = "#{version[:helm]}__#{version[:kubectl]}__#{version[:terraform]}".gsub('v', '')
-                buildArgs = "--build-arg HELM_VERSION=#{version[:helm]}" + \
-                    " --build-arg KUBECTL_VERSION=#{version[:kubectl]}" + \
-                    " --build-arg TERRAFORM_VERSION=#{version[:terraform]}"
-                image_name = "#{dir}:#{tag}"
-                run("docker build #{buildArgs} -t #{image_name} .")
-            end
-        end
+  dirs.each do |dir|
+    Dir.chdir(dir) do
+      versions.each do |version|
+        puts "Building version #{version}"
+        tag = "#{version[:helm]}__#{version[:kubectl]}__#{version[:terraform]}".gsub('v', '')
+        build_args = "--build-arg HELM_VERSION=#{version[:helm]}" + \
+                     " --build-arg KUBECTL_VERSION=#{version[:kubectl]}" + \
+                     " --build-arg TERRAFORM_VERSION=#{version[:terraform]}"
+        image_name = "#{dir}:#{tag}"
+        run("docker build #{build_args} -t #{image_name} .")
+      end
     end
+  end
+end
+
+def markdown(dirs, versions)
+  dirs.each do |dir|
+    Dir.chdir(dir) do
+      puts ''
+      puts "### #{dir}"
+      puts ''
+      puts '| Name | helm | kubectl | terraform |'
+      puts '|------|------|---------|-----------|'
+
+      versions.each do |version|
+        tag = "#{version[:helm]}__#{version[:kubectl]}__#{version[:terraform]}".gsub('v', '')
+        image_name = "#{dir}:#{tag}"
+        puts "| #{image_name} | #{version[:helm]} | #{version[:kubectl]} | #{version[:terraform]} |"
+      end
+    end
+  end
 end
 
 def deploy(dirs, versions)
-    dirs.each do |dir|
-        versions.each do |version|
-            puts "Deploying version #{version}"
-            tag = "#{version[:helm]}__#{version[:kubectl]}__#{version[:terraform]}".gsub('v', '')
-            image_name = "#{dir}:#{tag}"
-            public_image_name = "#{ENV['DOCKER_USERNAME']}/#{image_name}"
-            run("docker tag #{image_name} #{public_image_name}")
-            run("docker push #{public_image_name}")
-        end
+  dirs.each do |dir|
+    versions.each do |version|
+      puts "Deploying version #{version}"
+      tag = "#{version[:helm]}__#{version[:kubectl]}__#{version[:terraform]}".gsub('v', '')
+      image_name = "#{dir}:#{tag}"
+      public_image_name = "#{ENV['DOCKER_USERNAME']}/#{image_name}"
+      run("docker tag #{image_name} #{public_image_name}")
+      run("docker push #{public_image_name}")
     end
+  end
 end
 
 helm_versions = ['v2.8.0', 'v2.8.2', 'v2.11.0']
@@ -52,20 +70,22 @@ terraform_versions = ['0.11.8', '0.11.10']
 versions = []
 
 helm_versions.each do |h|
-    kubectl_versions.each do |k|
-        terraform_versions.each do |t|
-            versions.push({ helm: h, kubectl: k, terraform: t})
-        end
+  kubectl_versions.each do |k|
+    terraform_versions.each do |t|
+      versions.push(helm: h, kubectl: k, terraform: t)
     end
+  end
 end
 
 # find all top level folders, except hidden
 dirs = Dir.glob('*').select { |f| File.directory? f }
 
 if ARGV[0] == 'deploy'
-    login
-    deploy(dirs, versions)
-    logout
+  login
+  deploy(dirs, versions)
+  logout
+elsif ARGV[0] == 'markdown'
+  markdown(dirs, versions)
 else
-    build(dirs, versions)
+  build(dirs, versions)
 end
